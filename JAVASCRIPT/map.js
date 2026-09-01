a5c9e9f3e3eaac0d8a722e18b472dde0b2cfebee
Parimal-Mishra <parimalmishrawork@gmail.com>
2026-09-01 09:31:46 +0530

    js

--- JAVASCRIPT/map.js
+++ JAVASCRIPT/map.js
@@ -1,7 +1,9 @@
 const num=[11,22,35,48,54,9,79,81,94];
-const even=num.map(val => val%2==0);
+const even=num.filter(val => val%2==0);
+const sqr=even.map(val => val*val);
 console.log("num: "+num);
 console.log("even: "+even);
+console.log("square: "+sqr);
 
-//Difference for each and map is that for each is used to iterate over an array and perform an action on each element, 
-// while map is used to create a new array by transforming each element of the original array.
+//Difference for each and map is that for each is used to iterate over an array, 
+// while map is used to create a new array by performing an operation on each element of the original array.
